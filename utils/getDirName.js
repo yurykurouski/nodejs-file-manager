8ea8326dbec0e36c879676183db877cbeef1abb2
Yury Kurouski <yury_kurouski@epam.com>
2024-01-31 20:42:00 +0100

    feat: add getDirName util

---
+++ utils/getDirName.js
@@ -0,0 +1,9 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+export const getDirName = () => {
+  const __filename = fileURLToPath(import.meta.url);
+
+  return path.dirname(__filename);
+}
