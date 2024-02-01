e5e1a98cff2bd5b5984079a923f4db0cd3413114
Yury Kurouski <yury_kurouski@epam.com>
2024-02-01 01:06:18 +0100

    feat: add printFolderContent

---
+++ src/printFolderContent.js
@@ -0,0 +1,11 @@
+import { table } from 'console'
+import { getDirName } from './utils/getDirName.js';
+import { getSortedFolderContent } from './utils/getSortedFolderContent.js';
+
+export const printFolderContent = async () => {
+  const dirName = getDirName(import.meta.url);
+
+  const folderContent = await getSortedFolderContent(dirName);
+
+  table(folderContent);
+}
