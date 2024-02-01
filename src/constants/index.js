75f21cfe1419ce4b5e1ab7395bfa8d31a5c571d7
Yury Kurouski <yury_kurouski@epam.com>
2024-02-02 00:29:59 +0100

    feat: add constants for getOSInfo

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,11 @@
 export const ARG_USERNAME = '--username';
 
 export const COMMAND_LS = 'ls';
+export const COMMAND_UP = 'up';
+export const COMMAND_OS = 'os';
+
+export const COMMAND_ARG_EOL = '--EOL';
+export const COMMAND_ARG_CPUS = '--cpus'
+export const COMMAND_ARG_HOMEDIR = '--homedir'
+export const COMMAND_ARG_USERNAME = '--username'
+export const COMMAND_ARG_ARCH = '--architecture'
