b9365e3ceb53527745fd996ff67ced6137127a45
Yury Kurouski <yury_kurouski@epam.com>
2024-02-04 00:08:18 +0100

    refactor: use join

--- src/ops/file/readFile.js
+++ src/ops/file/readFile.js
@@ -1,9 +1,10 @@
 import fs from 'fs';
+import { join } from 'path';
 import { printError } from '../../utils/index.js';
 
 
 export const readFile = (path, currentDir) => {
-  const filePath = path[0] === '/' ? path : currentDir + `/${ path }`
+  const filePath = path[0] === '/' ? path : join(currentDir, path);
 
   const readableStream = fs.createReadStream(filePath);
 
