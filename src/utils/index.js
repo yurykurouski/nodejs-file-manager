7fa559b424b1bd224a0eb3d9a82c08db9300fd36
Yury Kurouski <yury_kurouski@epam.com>
2024-02-03 22:38:41 +0100

    chore: move printGreetings in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,4 @@ export { getDirName } from './getDirName.js';
 export { getSortedFolderContent } from './getSortedFolderContent.js';
 export { printCurrentDir } from './printCurrentDir.js';
 export { printError } from './printError.js';
+export { printGreetings } from './printGreetings.js';
