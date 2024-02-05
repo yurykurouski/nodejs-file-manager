feea24e4fa405219b3f301c733f1f49a2a2384ba
Yury Kurouski <yury_kurouski@epam.com>
2024-02-05 19:52:18 +0100

    feat: add err message, add empty path resolving

--- src/ops/zip/compressFile.js
+++ src/ops/zip/compressFile.js
@@ -1,17 +1,18 @@
-import zlib from 'zlib';
 import fs from 'fs';
+import zlib from 'zlib';
 import { basename, resolve } from 'path';
+import { ERROR_TYPE } from '../../constants/index.js';
 import { printError, printCurrentDir } from "../../utils/index.js";
 
 
 export const compressFile = ([sourceFile, targetDir], currDir) => {
-  if (!sourceFile || !targetDir) return printError();
+  if (!sourceFile) return printError(ERROR_TYPE);
 
   const fileName = basename(sourceFile);
 
   const compressStream = zlib.createBrotliCompress();
   const readableStream = fs.createReadStream(sourceFile);
-  const writeableStream = fs.createWriteStream(resolve(targetDir, `${ fileName }.gzip`));
+  const writeableStream = fs.createWriteStream(resolve(targetDir || currDir, `${ fileName }.gzip`));
 
   readableStream.pipe(compressStream).pipe(writeableStream);
 
