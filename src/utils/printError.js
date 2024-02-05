9e692166ffca9a94c8637370c8e2019b9ed99986
Yury Kurouski <yury_kurouski@epam.com>
2024-02-05 19:43:50 +0100

    feat: add default err message

--- src/utils/printError.js
+++ src/utils/printError.js
@@ -1,3 +1,5 @@
-export const printError = (errorMessage) => {
+import { ERROR_TYPE } from "../constants.index.js";
+
+export const printError = (errorMessage = ERROR_TYPE.OPERATION) => {
   console.log(errorMessage);
 }
